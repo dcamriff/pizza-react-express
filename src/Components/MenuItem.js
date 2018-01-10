557fccc2879d4f389c7c8bba3317c544d7b3108d
dcamriff <driffelatl@gmail.com>
2018-01-10 18:54:38 -0500

    fixed divs for MenuItem.js

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 // import pizzaData from '../pizzaData'
 
 class MenuItem extends Component {
     render() {
 
         return (
-            
-            <div id="menu" className="menu-item">
-            <div>
-            <img src={this.props.image} alt="image"/>
-            </div>
-            <h2>{this.props.name}</h2>
-            <h3>price: {this.props.price}</h3>
-            <p>{this.props.description}</p>
 
+            <div id="menu" className="menu-item">
+                <img src={this.props.image} alt="image"/>
+
+                <div className="menu-description">
+                    <div className="menu-item-name">
+                    {this.props.name}
+                    </div>
+                    <div>
+                        <strong>
+                        Price: {this.props.price}
+                        </strong>
+                    </div>
+
+                    <div>{this.props.description}
+                    </div>
+
+                </div>
             </div>
-            
+
         )
     }
 }
